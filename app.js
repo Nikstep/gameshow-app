4d67498e34ad9cb1efbcb131b043e446b625a3b5
mwiulsrod <martinwiulsrod@gmail.com>
2019-03-24 21:36:40 +0100

    Create a function for generating a random array of characters

---
+++ app.js
@@ -0,0 +1,35 @@
+// Get the element with the ID of qwerty and save it to a variable.
+const keyboardDiv = document.querySelector("#qwerty");
+
+// Get the element with the ID of phrase and save it to a variable.
+const phraseDiv = document.querySelector("#phrase");
+
+// Create a missed variable, initialized to 0, that you’ll use later to keep track of the number of guesses the player has missed.
+let missed = 0;
+
+// Attach a event listener to the “Start Game” button to hide the start screen overlay.
+const startGameButton = document.querySelector(".btn__reset");
+startGameButton.addEventListener("click", myFunction = () => {
+    document.querySelector("#overlay").style.display = "none";
+});
+
+// Create a phrases array that contains at least 5 different phrases as strings.
+const phrasesArray = ["Stick to your guns", "Slippery slopes", "Son of a gun", "Step on it", "Stick a fork in it"];
+
+// Create a getRandomPhraseAsArray function. It must take any given array of strings and return an array of characters.
+function getRandomPhraseAsArray(arr){ 
+    const max = arr.length;  
+    const index = Math.floor((Math.random() * max));
+    return Array.from(arr[index]);
+}
+const randomPhrase = getRandomPhraseAsArray(phrasesArray);
+
+// Set the game display.
+function addPhraseToDisplay(arr){
+    
+}
+
+addPhrasetoDisplay(getRandomPhraseAsArray(phrases)); 
+
+
+console.log("breakpoint");
